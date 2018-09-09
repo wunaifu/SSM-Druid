cacd3a14374e8039e5bde6c30b9c46d7560c5b28
wunaifu <1278022159@qq.com>
2018-09-09 20:41:25 +0800

    sql

---
+++ ssm-druid.sql
@@ -0,0 +1,42 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.5.51 : Database - ssm-druid
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ssm-druid` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `ssm-druid`;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id，自动增长',
+  `phone` varchar(15) NOT NULL COMMENT '手机（登录账号）',
+  `password` varchar(112) NOT NULL COMMENT '密码',
+  `nickname` varchar(15) DEFAULT NULL COMMENT '昵称',
+  `address` varchar(64) DEFAULT NULL COMMENT '联系地址',
+  `age` int(11) DEFAULT '0' COMMENT '年龄',
+  `sex` int(11) DEFAULT '1' COMMENT '性别（0女，1男）',
+  `user_info` varchar(150) DEFAULT NULL COMMENT '简介',
+  `headicon` varchar(200) DEFAULT NULL COMMENT '头像',
+  `register_time` varchar(32) DEFAULT NULL COMMENT '注册时间',
+  `permission` int(11) NOT NULL DEFAULT '0' COMMENT '权限（0普通用户，1可接单用户，2被禁用用户）',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `phone` (`phone`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;

2a558874438b7174247541ecf2919581b704e253
wunaifu <1278022159@qq.com>
2018-09-15 17:03:10 +0800

    redis缓存添加，中文乱码未解决

--- src/main/java/com.wnf/controller/PageController.java
+++ src/main/java/com.wnf/controller/PageController.java
@@ -1,11 +1,19 @@
 package com.wnf.controller;
 
+import com.wnf.jedis.JedisClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class PageController {
+
+    @Autowired
+    private JedisClient jedisClient;
 
     @RequestMapping("/")
     public String showIndex() {
@@ -15,6 +23,28 @@ public class PageController {
     @RequestMapping("/{page}")
     public String showPage(@PathVariable String page) {
         return page;
+    }
+
+    @ResponseBody
+    @RequestMapping("/jedis")
+    public String jedis() {
+        String json = "";
+        //先查询缓存
+        //添加缓存不能影响正常业务逻辑
+        try {
+            //查询缓存
+            json = jedisClient.hget("wnfSet", "wnf");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //把结果添加到缓存
+        try {
+            jedisClient.hset("wnfSet", "wnf", "测试，添加redis数据");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return json;
     }
 
 }
